cfca49a97b53b0d66e6340af257a3a4d18ddad03
Amaresh Kumar <amareshkumar7525@gmail.com>
2025-12-20 22:16:32 +0530

    Added : number swapping

---
+++ 100 basic problems/03_problem3.py
@@ -0,0 +1,12 @@
+# User will input (2numbers).Write a program to swap the numbers
+
+a = int(input("First Number : "))
+b = int(input("Second Number : "))
+
+c = a
+
+a = b
+b = c
+
+print("a is : ", a)
+print("b is : ", b)
