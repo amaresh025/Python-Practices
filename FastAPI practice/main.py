31b1c31a753ac0b8c5bbd6b0795d123a565d9eec
Amaresh Chauhan <amareshkumar7525@gmail.com>
2026-01-26 13:24:38 +0530

    Add files via upload

--- FastAPI practice/main.py
+++ FastAPI practice/main.py
@@ -1,5 +1,5 @@
 from modle import Product
-
+from fastapi import HTTPException
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,14 +12,21 @@ def root():
     return {"status": "everything is ok"}
 
 products = [
-    Product(id = 1, name = "laptop", price = 44000),
+    Product(id = 1, name = "laptop"),
     Product(id = 2, name = "phone", price = 14000)
 ]
-@app.get("/products")
+@app.get("/products/{id}")
 
-def get_product():
-    return products
+def get_product(id : int):
+    for item in range(len(products)):
+        if products[item].id == id:
+            return products[item]
+    raise HTTPException(
+        status_code=404,
+        detail="product not found" 
+    )
 
+    
 
 @app.post("/product")
 def add_products(product : Product):
